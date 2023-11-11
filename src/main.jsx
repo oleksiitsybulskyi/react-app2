a94d4fa4a24173ee75d5b8b4c82f3473f1819d10
Oleksii Tsybulskyi <oleksiitsybulskyi@gmail.com>
2023-11-10 20:59:25 -0600

    Step 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./page/error";
+import Home from "./page/Home";
+import Signup from "./page/Signup";
+import Login from "./page/Login";
+import Anotherpage from "./page/Anotherpage";
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home />, errorElement: <ErrorPage /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
+  { path: "/x", element: <Anotherpage /> },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
